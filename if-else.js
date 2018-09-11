4514402cfe661e7319fdfd6358f9ec1f12605773
akunmiq <41353484+akunmiq@users.noreply.github.com>
2018-09-11 11:05:53 +0700

    Update if-else.js

--- if-else.js
+++ if-else.js
@@ -10,16 +10,16 @@ else if(nama && peran === "") {
 }
 
 else if(nama && peran === "ksatria") {
-	console.log("Selamat datang di Dunia Proxitya, " +nama+)
+	console.log("Selamat datang di Dunia Proxitya, " +nama)
 	console.log("Halo Ksatria " +nama+ " , kamu dapat menyerang dengan senjatamu!")
 }
 
 else if(nama && peran === "tabib") {
-	console.log("Selamat datang di Dunia Proxitya, " +nama+)
+	console.log("Selamat datang di Dunia Proxitya, " +nama)
 	console.log("Halo Tabib " +nama+ " , kamu dapat membantu temanmu yang tereluka")
 }
 
 else if(nama && peran === "penyihir") {
-	console.log("Selamat datang di Dunia Proxytia, " +nama+)
+	console.log("Selamat datang di Dunia Proxytia, " +nama)
 	console.log("Halo penyihir " +nama+ " , ciptakan keajaiban yang membantu kemenanganmu!")
 }
